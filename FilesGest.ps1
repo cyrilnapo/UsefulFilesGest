977cb10c149eb8a97ebf00f4b961ec0d4fe39e23
napo <napoleone.cyril@gmail.com>
2024-01-12 09:26:07 +0100

    Finition gestion doublons + début allégement

--- FilesGest.ps1
+++ FilesGest.ps1
@@ -27,6 +27,20 @@ Repertoire dans lequel le gestionnaire va agir
  -
  
 .EXAMPLE
+./filesgest.ps1
+
+Que voulez vous faire ?
+
+        a) Chercher via criteres
+        b) Gestion des doublons de contenu
+        c) Allegement du repertoire
+
+        d) Quitter
+
+    >:b
+
+    No identical files detected !
+
 
 #>
 
@@ -36,7 +50,6 @@ param(
 )
 
 if(Test-Path $path){
-    $files = Get-ChildItem $path -Recurse
     clear-host
 }else{
     Clear-Host
@@ -62,19 +75,24 @@ while(1){
 
         }
 
+        #Gestion des doublons
         "b"{
             clear-host
+            $files = Get-ChildItem $path -Recurse
             $identicalFiles = @()
             $nbIdentiticalFiles = 0
 
+            #Comparaison de chaques fichiers entre eux
             for ($i = 0; $i -lt $files.Count; $i++) {
+                write-host "Analyse du répertoire en cours...  ($i/$($files.count))" -foregroundcolor darkyellow
                 for ($j = $i + 1; $j -lt $files.Count; $j++) {
+                    
                     #verifie si c'est un dossier ou un fichier, si dossier alors ne pas faire get-content
                     $verif1 = get-item $files[$i].FullName
-                    $verif2 = get-item $files[$j].FullName 
+                    $verif2 = get-item $files[$j].FullName
 
                     if($verif1 -is [system.io.fileinfo] -and $verif2 -is [system.io.fileinfo]){
-                        $file1 = Get-Content $files[$i].FullName
+                        $file1 = Get-Content $files[$i].FullName 
                         $file2 = Get-Content $files[$j].FullName 
                 
                         # Comparez le contenu des fichiers
@@ -82,43 +100,68 @@ while(1){
                             $identicalFiles += @{
                                 File1 = $files[$i].BaseName
                                 File2 = $files[$j].BaseName
+                                File1Root = $files[$i].Fullname
+                                File2Root = $files[$j].Fullname
                             }
-                            $nbIdentiticalFiles += 0.5
+                            $nbIdentiticalFiles += 1
                         }
                     }
-                    
-                    
                 }
             }
+            #Aucun doublons
             if($nbIdentiticalFiles -eq 0){
-                write-host "No identical files detected !" -ForegroundColor Green
+                clear-host
+                write-host "Aucun doublons détectés !" -ForegroundColor Green
             }else{
-                write-host "$nbIdentiticalFiles identical files has been detected ! (see below)" -ForegroundColor Red
+                #Doublons détectés
+                clear-host
+                write-host "$nbIdentiticalFiles Doublons détectés ! (voir ci-dessous)" -ForegroundColor Red
 
+                #Affichage des doublons
                 foreach($pair in $identicalFiles){
                     write-host "       - $($pair.File1) and $($pair.File2)" -ForegroundColor Red
                 }
+
+                #Choix de suppression ou non
                 do{
-                    $deleteChoice = read-host "`nVoulez vous supprimer les fichiers doublons ? Y/N" 
+                    $deleteChoice = read-host "`nVoulez vous supprimer les fichiers doublons ? O/N" 
 
                 }while($deleteChoice -ne "y" -and $deleteChoice -ne "n")
+
                 switch ($deleteChoice){
-                    "y"{
+                    #L'utilisateur veut supprimer ses doublons
+                    "o"{
                         clear-host
-                        write-host "Suppression des doublons en cours..." -foregroundcolor darkyellow
-                        #suppression ici
+                        #Suppression des fichiers doublons
+                        foreach($pair in $identicalfiles){
+                            remove-item $pair.File1Root -ErrorAction:Ignore
+                            remove-item $pair.File2Root -ErrorAction:Ignore
+                            write-host "Suppression des doublons en cours..." -foregroundcolor darkyellow
+                        }
+                        write-host "Doublons supprimés avec succès !" -foregroundcolor green
                     }
+                    #L'utilisateur ne désire pas supprimer ses doublons
                     "n"{
                         clear-host
                         write-host "Doublons non-supprimés `n" -foregroundcolor blue
                     }
                 }
-                
             }
-            
         }
 
         "c"{
+            clear-host
+            $unusedHeavyFiles = Get-Childitem $path -recurse -file | sort-object length, LasteWriteTime | select-object -last 10
+            write-host "Voici une séléction des fichiers qui sont les plus lourds de votre répertoire et que vous utilisez le moins :`n"
+            foreach($file in $unusedHeavyFiles){
+                write-host "   - $($file.Name)" -ForegroundColor darkyellow -nonewline
+                write-host " Taille : " -nonewline -foregroundcolor blue
+                write-host "$($file.length) " -nonewline -foregroundcolor green
+                write-host " Dernière utilisation : " -nonewline -foregroundcolor blue
+                write-host "$($file.lastWriteTime)" -foregroundcolor green
+            }
+            
+            
 
         }
 
